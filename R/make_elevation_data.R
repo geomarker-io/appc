b746d22bdcae0b7eca0841586cb331a618c24d76
Cole Brokamp <cole.brokamp@gmail.com>
2023-12-18 22:50:09 -0500

    simplify elevation code

--- R/make_elevation_data.R
+++ R/make_elevation_data.R
@@ -1,31 +1,44 @@
-library(dplyr)
-library(s2)
-library(terra)
+install_elevation_data <- function() {
+  dest_file <- fs::path(tools::R_user_dir("appc", "data"), "PRISM_us_dem_800m_bil.bil")
+  if (fs::file_exists(dest_file)) return(dest_file)
+  elevation_zip <- tempfile("elevation", fileext = ".zip")
+  paste0("https://prism.oregonstate.edu/fetchData.php",
+         "?type=bil&kind=normals&spatial=800m&elem=dem&temporal=annual") |>
+    download.file(destfile = elevation_zip)
+  unzip(elevation_zip, exdir = tools::R_user_dir("appc", "data"))
+  return(dest_file)
+}
 
-# TODO automate download from https://prism.oregonstate.edu/normals/
-elevation_raster <- rast("./PRISM_us_dem_800m_bil/PRISM_us_dem_800m_bil.bil")
+#' get elevation summary data
+#' @param x a vector of s2 cell identifers (`s2_cell` object)
+#' @param fun function to summarize extracted data
+#' @param buffer distance from s2 cell (in meters) to summarize data
+get_elevation_summary <- function(x, fun = median, buffer = 800) {
+  if (!inherits(x, "s2_cell")) stop("x must be a s2_cell vector", call. = FALSE)
+  elevation_raster <- terra::rast(install_elevation_data())
+  x_vect <-
+    tibble::tibble(
+      s2 = unique(x),
+      s2_geography = s2::s2_buffer_cells(s2::s2_cell_to_lnglat(s2), distance = buffer)
+    ) |>
+    sf::st_as_sf() |>
+    terra::vect() |>
+    terra::project(elevation_raster)
+  elevations <-
+    terra::extract(elevation_raster, x_vect, fun = fun)$PRISM_us_dem_800m_bil |>
+    as.list() |>
+    setNames(x_vect$s2)
+  elevations[as.character(x)]
+}
+
+library(s2)
+library(dplyr, warn.conflicts = FALSE)
 
 d <-
   arrow::read_parquet("data/aqs.parquet") |>
-  select(s2) |>
-  mutate(s2_geography = as_s2_geography(s2_cell_to_lnglat(s2)))
+  select(s2)
 
-d_vect <-
-  d |>
-  mutate(s2_geography = s2_buffer_cells(s2_geography, distance = 800)) |>
-  sf::st_as_sf() |>
-  vect() |>
-  terra::project(elevation_raster)
+d$elevation_median_800 <- get_elevation_summary(x = d$s2, fun = median, buffer = 800)
+d$elevation_sd_800 <- get_elevation_summary(x = d$s2, fun = sd, buffer = 800)
 
-# average and sd of elevation within 400 m buffer radius circle
-d$elevation <-
-  terra::extract(elevation_raster, d_vect, fun = median, ID = FALSE) |>
-  pull(PRISM_us_dem_800m_bil)
-
-d$elevation_sd <-
-  terra::extract(elevation_raster, d_vect, fun = sd, ID = FALSE) |>
-  pull(PRISM_us_dem_800m_bil)
-
-d |>
-  select(-s2_geography) |>
-  arrow::write_parquet("data/elevation.parquet")
+write_parquet(d, "data/elevation.parquet")
