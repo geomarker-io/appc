7b12e246afd803a7cd0bdab95127f57d2efd0a13
Cole Brokamp <cole.brokamp@gmail.com>
2023-12-22 23:08:28 -0500

    functionize NEI data for two different vintages

--- R/nei.R
+++ R/nei.R
@@ -1,54 +1,76 @@
-library(dplyr)
-library(s2)
-library(readr)
-
-
-# TODO update to get every three years release
-make_nei_data <- function() {
-  # TODO automate 'pinning' downloaded data
-  ## download.file("https://gaftp.epa.gov/air/nei/2020/data_summaries/Facility%20Level%20by%20Pollutant.zip",
-  ##   destfile = "data-raw/nei_2020.zip"
-  ## )
-  ## unzip("data-raw/2020_nei.zip")
-  read_csv("data-raw/Facility Level by Pollutant/emis_sum_fac_23959.csv",
-    col_types = cols_only(
-      `site latitude` = col_double(),
-      `site longitude` = col_double(),
-      `pollutant code` = col_character(),
-      `total emissions` = col_double(),
-      `emissions uom` = col_character(),
-    )
-  ) |>
-    filter(`pollutant code` %in%
-      c("EC", "OC", "SO4", "NO3", "PMFINE", "PM25-PRI")) |>
-    mutate(s2 = as_s2_cell(s2_geog_point(`site longitude`, `site latitude`))) |>
-    select(-`site latitude`, -`site longitude`) |>
-    rename_with(~ tolower(gsub(" ", "_", .x, fixed = TRUE))) |>
-    na.omit()
+#' installs NEI point data into user's data directory for the `appc` package
+#' @param year a character string that is the year of the data
+#' @return path to NEI point data parquet file
+#' @details The NEI file is downloaded, unzipped, and filtered to observations
+#' with a pollutant code of `EC`, `OC`, `SO4`, `NO3`, `PMFINE`, or `PM25-PRI`.
+#' Latitude and longitude are encoded as an s2 vector, column names are cleaned,
+#' and rows with missing values (including total emissions or emissions units) are excluded.
+install_nei_point_data <- function(year = as.character(c(2020, 2017))) {
+  year <- rlang::arg_match(year)
+  dest_file <- fs::path(tools::R_user_dir("appc", "data"), glue::glue("nei_{year}.parquet"))
+  if (file.exists(dest_file)) return(dest_file)
+  message(glue::glue("downloading {year} NEI file"))
+  zip_path <- fs::path(tempdir(), glue::glue("nei_{year}.zip"))
+  dplyr::case_when(year == "2020" ~ "https://gaftp.epa.gov/air/nei/2020/data_summaries/Facility%20Level%20by%20Pollutant.zip",
+                   year == "2017" ~ "https://gaftp.epa.gov/air/nei/2017/data_summaries/2017v1/2017neiJan_facility.zip") |>
+    httr::GET(httr::write_disk(zip_path), httr::progress(), overwrite = TRUE)
+  nei_raw_paths <- unzip(zip_path, exdir = tempdir())
+  grep(".csv", nei_raw_paths, fixed = TRUE, value = TRUE) |>
+    readr::read_csv(col_types = readr::cols_only(
+      `site latitude` = readr::col_double(),
+      `site longitude` = readr::col_double(),
+      `pollutant code` = readr::col_character(),
+      `total emissions` = readr::col_double(),
+      `emissions uom` = readr::col_character()
+    )) |>
+    dplyr::filter(`pollutant code` %in%
+                    c("EC", "OC", "SO4", "NO3", "PMFINE", "PM25-PRI")) |>
+    dplyr::mutate(s2 = s2::as_s2_cell(s2::s2_geog_point(`site longitude`, `site latitude`))) |>
+    dplyr::select(-`site latitude`, -`site longitude`) |>
+    dplyr::rename_with(~ tolower(gsub(" ", "_", .x, fixed = TRUE))) |>
+    na.omit() |>
+    arrow::write_parquet(dest_file)
+  return(dest_file)
 }
 
-d_nei <- make_nei_data()
+#' get NEI point summary data
+#' @param x a vector of s2 cell identifers (`s2_cell` object)
+#' @param year a character string that is the year of the NEI data
+#' @param pollutant_code the NEI pollutant to summarize
+#' @param buffer distance from s2 cell (in meters) to summarize data
+#' @return a vector (the same length as `x`) of the sum of all point emissions within the buffer distance weighted
+#' by the inverse of the distance squared to each emission point
+get_nei_point_summary <- function(x, year, pollutant_code = c("PM25-PRI", "EC", "OC", "SO4", "NO3", "PMFINE"), buffer = 1000) {
+  if (!inherits(x, "s2_cell")) stop("x must be a s2_cell vector", call. = FALSE)
+  nei_data <- arrow::read_parquet(install_nei_point_data(year = year))
+  pollutant_code <- rlang::arg_match(pollutant_code)
 
-d <- arrow::read_parquet("data/aqs.parquet")
+  message("intersecting nei point sources")
+  withins <- s2::s2_dwithin_matrix(s2::s2_cell_to_lnglat(x), s2::s2_cell_to_lnglat(nei_data$s2), distance = 1000)
 
-message("intersecting nei point sources")
-d$withins <- s2_dwithin_matrix(s2_cell_to_lnglat(d$s2), s2_cell_to_lnglat(d_nei$s2), distance = 1000)
+  summarize_emissions <- function(i) {
+    nei_data[withins[[i]], ] |>
+      dplyr::filter(pollutant_code == pollutant_code) |>
+      dplyr::mutate(
+        dist_to_point =
+          purrr::map_dbl(s2, \(.) s2::s2_cell_distance(., x[[i]]))
+      ) |>
+      dplyr::summarize(nei_pm25_id2w = sum(total_emissions / dist_to_point^2)) |>
+      as.double()
+  }
 
-summarize_emissions <- function(i) {
-  d_nei[d$withins[[i]], ] |>
-    filter(pollutant_code == "PM25-PRI") |>
-    mutate(
-      dist_to_point =
-        purrr::map_dbl(s2, \(.) s2_cell_distance(., d$s2[[i]]))
-    ) |>
-    summarize(nei_pm25_id2w = sum(total_emissions / dist_to_point^2)) |>
-    as.double()
+  nei_pollutant_id2w <- purrr::map_dbl(1:length(withins), summarize_emissions, .progress = "summarizing intersected nei point sources")
+  return(nei_pollutant_id2w)
 }
 
-d$nei_pm25_id2w <- purrr::map_dbl(1:nrow(d), summarize_emissions, .progress = "summarizing intersected nei point sources")
+d <-
+  arrow::read_parquet("data/aqs.parquet") |>
+  dplyr::distinct(s2)
+
+d$nei_2020_pm25_1000 <- get_nei_point_summary(d$s2, "2020")
+d$nei_2017_pm25_1000 <- get_nei_point_summary(d$s2, "2017")
+
 
 # TODO add non-point sources
 
-d |>
-  select(s2, pollutant, nei_pm25_id2w) |>
-  arrow::write_parquet("data/nei.parquet")
+arrow::write_parquet(d, "data/nei.parquet")
