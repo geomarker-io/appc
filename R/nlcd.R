6a6421b3d17a4954d2ebb3039a1143412ee191cb
Cole Brokamp <cole.brokamp@gmail.com>
2023-12-26 14:54:39 -0500

    update how output nlcd is structured

--- R/nlcd.R
+++ R/nlcd.R
@@ -8,7 +8,7 @@ install_impervious <- function(year = as.character(c(2019, 2016, 2013, 2011, 200
   message(glue::glue("downloading {year} NLCD impervious raster"))
   nlcd_zip_path <- fs::path(tempdir(), glue::glue("nlcd_impervious_{year}.zip"))
   glue::glue("https://s3-us-west-2.amazonaws.com/mrlc/nlcd_{year}_impervious_l48_20210604.zip") |>
-    httr::GET(httr::write_disk(nlcd_zip_path), httr::progress(), overwrite = TRUE)
+    httr::GET(httr::write_disk(nlcd_zip_path, overwrite = TRUE), httr::progress())
   nlcd_raw_paths <- unzip(nlcd_zip_path, exdir = tempdir())
   message(glue::glue("converting {year} NLCD impervious raster"))
   system2(
@@ -19,27 +19,6 @@ install_impervious <- function(year = as.character(c(2019, 2016, 2013, 2011, 200
   )
   return(dest_file)
 }
-
-#' get imperviou summary data
-#' @param x a vector of s2 cell identifers (`s2_cell` object)
-#' @param year a character string that is the year of the imperviou data
-#' @param fun function to summarize extracted data
-#' @param buffer distance from s2 cell (in meters) to summarize data
-#' @return a vector of imperviou values (the same length as `x`)
-get_impervious_summary <- function(x, year, fun = mean, buffer = 400) {
-  if (!inherits(x, "s2_cell")) stop("x must be a s2_cell vector", call. = FALSE)
-  impervious_raster <- terra::rast(install_impervious(year = year))
-  x_vect <-
-    tibble::tibble(
-      s2 = unique(x),
-      s2_geography = s2::s2_buffer_cells(s2::s2_cell_to_lnglat(s2), distance = buffer)
-    ) |>
-    sf::st_as_sf() |>
-    terra::vect() |>
-    terra::project(impervious_raster)
-  xx <- terra::extract(impervious_raster, x_vect, fun = fun, ID = FALSE)$Layer_1
-  setNames(xx, as.character(x_vect$s2))[as.character(x)]
-  }
 
 #' installs NLCD treecanopy raster data into user's data directory for the `appc` package
 #' @param year a character string that is the year of the data
@@ -64,15 +43,22 @@ install_treecanopy <- function(year = as.character(2021:2011)) {
   return(dest_file)
 }
 
-#' get treecanopy summary data
+
+#' get mean NLCD values buffered around a point
 #' @param x a vector of s2 cell identifers (`s2_cell` object)
-#' @param year a character string that is the year of the treecanopy data
-#' @param fun function to summarize extracted data
+#' @param product
+#' @param year a character string that is the year of the impervious or treecanopy data;
+#' note that each product has different available years
 #' @param buffer distance from s2 cell (in meters) to summarize data
-#' @return a vector of treecanopy values (the same length as `x`)
-get_treecanopy_summary <- function(x, year, fun = mean, buffer = 400) {
+#' @return a vector of mean impervious or treecanopy values (the same length as `x`)
+get_nlcd_summary <- function(x, product = c("impervious", "treecanopy"), year, buffer = 400) {
   if (!inherits(x, "s2_cell")) stop("x must be a s2_cell vector", call. = FALSE)
-  treecanopy_raster <- terra::rast(install_treecanopy(year = year))
+  product <- rlang::arg_match(product)
+  the_raster <-
+    glue::glue("install_{product}(year = '{year}')") |>
+    rlang::parse_expr() |>
+    eval() |>
+    terra::rast()
   x_vect <-
     tibble::tibble(
       s2 = unique(x),
@@ -80,29 +66,28 @@ get_treecanopy_summary <- function(x, year, fun = mean, buffer = 400) {
     ) |>
     sf::st_as_sf() |>
     terra::vect() |>
-    terra::project(treecanopy_raster)
-  xx <- terra::extract(treecanopy_raster, x_vect, fun = fun, ID = FALSE)$Layer_1
+    terra::project(the_raster)
+  xx <- terra::extract(the_raster, x_vect, fun = mean, ID = FALSE)$Layer_1
   setNames(xx, as.character(x_vect$s2))[as.character(x)]
-  }
-
+}
+  
 d <-
   arrow::read_parquet("data/aqs.parquet") |>
   dplyr::distinct(s2)
 
-d$treecanopy_2019 <- get_treecanopy_summary(d$s2, year = "2019")
-d$impervious_2019 <- get_impervious_summary(d$s2, year = "2019")
-d$treecanopy_2016 <- get_treecanopy_summary(d$s2, year = "2016")
+impervious_years <- c("2016", "2019")
+purrr::walk(impervious_years, install_impervious)
+d$impervious_400 <-
+  purrr::map(c("2016", "2019"), \(x) get_nlcd_summary(d$s2, product = "impervious", year = x, buffer = 400)) |>
+  setNames(impervious_years) |>
+  purrr::list_transpose()
 
-# TODO run all of these
-d$treecanopy_2017 <- get_treecanopy_summary(d$s2, year = "2017")
-d$treecanopy_2018 <- get_treecanopy_summary(d$s2, year = "2018")
-d$treecanopy_2020 <- get_treecanopy_summary(d$s2, year = "2020")
-d$treecanopy_2021 <- get_treecanopy_summary(d$s2, year = "2021")
-d$impervious_2016 <- get_impervious_summary(d$s2, year = "2016")
-
-# TODO way to nest these into list of vectors, with each vector element named by the year it represents?
-
-# TODO don't repeat dates and conc columns
+treecanopy_years <- as.character(2021:2016)
+purrr::walk(treecanopy_years, install_treecanopy)
+d$treecanopy_400 <-
+  purrr::map(treecanopy_years, \(x) get_nlcd_summary(d$s2, product = "treecanopy", year = x, buffer = 400)) |>
+  setNames(treecanopy_years) |>
+  purrr::list_transpose()
 
 arrow::write_parquet(d, "data/nlcd.parquet")
 
