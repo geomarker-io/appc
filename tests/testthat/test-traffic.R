5d3adfe4b615a2600729fbc25284a6221373a475
Cole Brokamp <cole.brokamp@gmail.com>
2024-02-07 11:43:44 -0500

    fix traffic tests for 14 interp on traffic

--- tests/testthat/test-traffic.R
+++ tests/testthat/test-traffic.R
@@ -1,15 +1,21 @@
 test_that("get_traffic_summary works", {
   get_traffic_summary(x = s2::as_s2_cell(c("8841b399ced97c47", "8841b38578834123"))) |>
-    expect_equal(list(
-      `8841b399ced97c47` = list(aadt_total_m = 0, aadt_truck_m = 0),
-      `8841b38578834123` = list(aadt_total_m = 0, aadt_truck_m = 0)
-    ))
-
+    expect_equal(
+      list(`8841b399ced97c47` = list(
+        aadt_total_m = 16697223.3895544,
+        aadt_truck_m = 14454627503016.2
+      ), `8841b38578834123` = list(
+        aadt_total_m = 33445517.3970626,
+        aadt_truck_m = 28953466354329.6
+      ))
+    )
   get_traffic_summary(s2::as_s2_cell(c("8841b399ced97c47", "8841b38578834123")), buffer = 1500) |>
-    expect_equal(list(`8841b399ced97c47` = list(
-      aadt_total_m = 659553870.545063,
-      aadt_truck_m = 481087361966862
-    ), `8841b38578834123` = list(
-      aadt_total_m = 252742369.39041, aadt_truck_m = 281313235601750
-    )))
+    expect_equal(
+      list(`8841b399ced97c47` = list(
+        aadt_total_m = 657047536.321914,
+        aadt_truck_m = 430564143157840
+      ), `8841b38578834123` = list(
+        aadt_total_m = 225667410.451303,
+        aadt_truck_m = 201099749546257
+      )))
 })
