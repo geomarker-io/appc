9194d16acc6ca97188638843a42ed55647a3e69a
Cole Brokamp <cole.brokamp@gmail.com>
2024-02-07 11:18:28 -0500

    change traffic to level 14 s2 approximation to increase speed

--- R/traffic.R
+++ R/traffic.R
@@ -22,7 +22,7 @@ get_traffic_summary <- function(x, buffer = 400) {
       aadt_total = AADT,
       aadt_truck = sum(AADT_SINGLE_UNIT, AADT_COMBINATION, na.rm = TRUE)
     ) |>
-    dplyr::group_by(s2_parent = s2::s2_cell_parent(s2_centroid, level = 15)) |>
+    dplyr::group_by(s2_parent = s2::s2_cell_parent(s2_centroid, level = 14)) |>
     dplyr::summarize(
       aadt_total_m = sum(aadt_total * length, na.rm = TRUE),
       aadt_truck_m = sum(aadt_truck * length, na.rm = TRUE)
@@ -32,7 +32,7 @@ get_traffic_summary <- function(x, buffer = 400) {
   # s2 level 15 are 260 m sq
   # s2 level 14 are 521 m sq
   xx <- unique(x)
-  message("intersecting with AADT data using level 15 s2 approximation ( ~ 260 sq m)")
+  message("intersecting with AADT data using level 14 s2 approximation ( ~ 260 sq m)")
   withins <- s2::s2_dwithin_matrix(s2::s2_cell_to_lnglat(xx), s2::s2_cell_to_lnglat(aadt_data$s2_parent), distance = buffer)
   summarize_traffic <- function(i) {
     aadt_data[withins[[i]], ] |>
