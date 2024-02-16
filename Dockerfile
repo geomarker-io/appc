00bce028ed3986588c0c3f247e4a8d2b2c975895
Cole Brokamp <cole.brokamp@gmail.com>
2024-02-16 09:38:10 -0500

    test and fix bug in NARR if dates argument was not named

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,21 @@ FROM ghcr.io/rocker-org/r-ver:4.3.1
 RUN apt-get update \
     && apt-get install -yqq --no-install-recommends \
     libcurl4-openssl-dev \
+    gdal-bin libgdal-dev libgeos-dev \
+    libicu-dev libnetcdf-dev libproj-dev \
+    libssl-dev libudunits2-dev \
     && apt-get clean
 
 RUN R --quiet -e "install.packages('pak', repos = c(CRAN = 'https://packagemanager.rstudio.com/all/__linux__/focal/latest'))"
 
-RUN R --quiet -e "pak::pak('geomarker-io/appc')"
+COPY DESCRIPTION ./DESCRIPTION
+
+RUN R --quiet -e "pak::local_install_dev_deps()"
+
+COPY ./ /appc/
+
+RUN R CMD build appc
+
+RUN R CMD INSTALL appc_*.tar.gz
+
+RUN R CMD check --as-cran --no-manual --no-vignettes appc_*.tar.gz
--- R/narr.R
+++ R/narr.R
@@ -29,11 +29,9 @@ get_narr_data <- function(x, dates, narr_var = c("air.2m", "hpbl", "acpcp", "rhu
     terra::vect(geom = c("x", "y"), crs = "+proj=longlat +datum=WGS84") |>
     terra::project(narr_raster)
   narr_cells <- terra::cells(narr_raster[[1]], x_vect)[, "cell"]
-  xx <- terra::extract(narr_raster, narr_cells)
-  purrr::imap(dates,
-    \(x, idx) unlist(xx[idx, as.character(x)]),
-    .progress = paste0("calculating ", narr_var)
-  )
+  xx <- as.data.frame(t(terra::extract(narr_raster, narr_cells)))
+  purrr::map2(1:ncol(xx), dates, \(.x, .y) xx[as.character(.y), .x]) |>
+    stats::setNames(as.character(x))
 }
 
 #' Installs NARR raster data into user's data directory for the `appc` package
