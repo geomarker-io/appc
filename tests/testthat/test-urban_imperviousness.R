b44ecf06eec5b6714a7b25048dddd75914f828ba
Cole Brokamp <cole.brokamp@gmail.com>
2024-02-15 23:27:39 -0500

    will need to make *all* files rds ?

--- tests/testthat/test-urban_imperviousness.R
+++ tests/testthat/test-urban_imperviousness.R
@@ -1,3 +1,4 @@
+skip_on_os("windows")
 test_that("get_urban_imperviousness works", {
   get_urban_imperviousness(x = s2::as_s2_cell(c("8841b399ced97c47", "8841b38578834123")), year = "2021") |>
     expect_equal(c(`8841b399ced97c47` = 74.3225806451613, `8841b38578834123` = 33.6276978417266))
