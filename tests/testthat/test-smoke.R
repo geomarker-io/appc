f05841333ebb097cbea50d06d2c721ada07788eb
Cole Brokamp <cole.brokamp@gmail.com>
2024-02-24 17:02:55 -0500

    deprecate smoke

--- tests/testthat/test-smoke.R
+++ tests/testthat/test-smoke.R
@@ -8,15 +8,3 @@ test_that("get_hms_smoke_data() works", {
     expect_identical(list(c(9, 0), c(0, 2)))
 
 })
-
-test_that("install_smoke_pm_data() works", {
-  d <- list(
-    "8841b39a7c46e25f" = as.Date(c("2023-05-18", "2023-11-06")),
-    "8841a45555555555" = as.Date(c("2023-06-22", "2023-08-15"))
-  )
-  d_smoke <- readRDS(install_smoke_pm_data())
-  d_smoke |>
-    expect_named() |>
-    expect_s3_class("tbl") |>
-    expect_length(3)
-})
