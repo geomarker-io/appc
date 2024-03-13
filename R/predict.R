55406cc6fa3585f41e306774f1c415ca09a06f45
Cole Brokamp <cole.brokamp@gmail.com>
2024-03-12 23:10:58 -0400

    visible binding fix

--- R/predict.R
+++ R/predict.R
@@ -50,6 +50,7 @@ predict_pm25 <- function(x, dates) {
 utils::globalVariables(c("air.2m", "hpbl", "acpcp", "rhum.2m",
                          "vis", "pres.sfc", "uwnd.10m", "vwnd.10m",
                          "urban_imperviousness_400",
+                         "merra_pm25",
                          "plume_smoke", ".rowid",
                          "nei_point_id2w_1000", "census_tract_id_2010",
                          "predictions", "variance.estimates"))
