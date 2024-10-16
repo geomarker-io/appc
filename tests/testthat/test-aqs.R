d4a15d240f88c12caaff414bb390a8738f2458f7
Cole Brokamp <cole.brokamp@gmail.com>
2024-10-16 08:12:13 -0400

    don't expect specific number of rows from aqs functions

--- tests/testthat/test-aqs.R
+++ tests/testthat/test-aqs.R
@@ -3,15 +3,12 @@ skip_if(is.null(curl::nslookup("aqs.epa.gov", error = FALSE)), "EPA AQS not onli
 
 test_that("get_daily_aqs() works", {
   aqs_pm <- get_daily_aqs("pm25", "2023")
-  expect_equal(nrow(aqs_pm), 33283)
   expect_equal(names(aqs_pm), c("s2", "date", "pollutant", "conc"))
   expect_s3_class(aqs_pm, c("tbl_df", "tbl", "data.frame"))
   aqs_ozone <- get_daily_aqs("ozone", "2022")
-  expect_equal(nrow(aqs_ozone), 364965)
   expect_equal(names(aqs_ozone), c("s2", "date", "pollutant", "conc"))
   expect_s3_class(aqs_ozone, c("tbl_df", "tbl", "data.frame"))
   aqs_no2 <- get_daily_aqs("ozone", "2021")
-  expect_equal(nrow(aqs_no2), 373208)
   expect_equal(names(aqs_no2), c("s2", "date", "pollutant", "conc"))
   expect_s3_class(aqs_no2, c("tbl_df", "tbl", "data.frame"))
 })
