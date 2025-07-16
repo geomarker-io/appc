3929d125d7f7281e80afb29d42b194a7d6d78a32
Cole Brokamp <cole.brokamp@gmail.com>
2025-07-16 16:01:23 -0400

    getting there wip

--- inst/make_traffic_data_release.R
+++ inst/make_traffic_data_release.R
@@ -2,153 +2,51 @@ library(sf)
 library(s2)
 library(dplyr, warn.conflicts = FALSE)
 
-traffic_release_name <- "hpms_f123_aadt"
-traffic_release_date <- Sys.Date()
-
 # download open HPMS geodatabase from arcgis:
 # https://www.arcgis.com/home/item.html?id=75f897d0151b409baea47a0f3544b75e
 # 5+ GB (!)
-download.file(
-  "https://www.arcgis.com/sharing/rest/content/items/c199f2799b724ffbacf4cafe3ee03e55/data",
-  "~/Downloads/HPMS_2020.gdb.zip"
-)
-unzip("~/Downloads/HPMS_2020.gdb.zip", exdir = "~/Downloads")
-
-gdb_path <- "~/Downloads/HPMS_2020"
-# using f_system of 1 or 2 would be all "interstates, freeways, and expressways"
-# F_SYSTEM: functional classification system:
-#   - 1: interstate
-#   - 2: principal arterial - other freeways and expressways
-#   - 3: principal arterial - other
-
-# AADT: FHWA 1-13
-# AADT_SINGLE_UNIT; aadt_trucks_buses: single-unit trucks and buses (FHWA 4-7)
-# AADT_COMBINATION; aadt_tractor_trailer: combination trucks (FHWA 8-13)
-# aadt_passenger: passenger (FHWA 1-3; AADT - AADT_SINGLE_UNIT - AADT_COMBINATION)
+gdb_path <- "~/Downloads/HPMS_2020.gdb"
+if (!file.exists(gdb_path)) {
+  download.file(
+    "https://www.arcgis.com/sharing/rest/content/items/c199f2799b724ffbacf4cafe3ee03e55/data",
+    "~/Downloads/HPMS_2020.gdb.zip"
+  )
+  unzip("~/Downloads/HPMS_2020.gdb.zip", exdir = "~/Downloads")
+}
 
 hpms_states <-
   st_layers(dsn = gdb_path)$name |>
   strsplit("_", fixed = TRUE) |>
   purrr::map_chr(3)
 
-hpms_state <- "OH"
-
-hpms_state_d <-
-  st_read(
-    dsn = gdb_path,
-    query = glue::glue(
-      "SELECT County_Code, Route_ID, AADT, AADT_SINGLE_UNIT, AADT_COMBINATION",
-      "FROM HPMS_FULL_{hpms_state}_2020",
-      "WHERE F_SYSTEM IN ('1', '2')",
-      .sep = " "
-    ),
-    quiet = TRUE
-  ) |>
-  na.omit() |>
-  st_zm() |>
-  tibble::as_tibble() |>
-  # filter(Route_ID == "SHAMIR00075**C") |>
-  # filter(County_Code == 61) |>
-  st_as_sf() |>
-  st_transform(5072)
-
-target_lvl <- 16
-
-hpms_state_d$s2_covering_cells_buffer <-
-  s2_covering_cell_ids(
-    as_s2_geography(st_buffer(hpms_state_d$Shape, dist = 400)),
-    min_level = target_lvl,
-    max_level = target_lvl,
-    max_cells = 1000
-  )
-
-state_aadt_s2_target_lvl <-
-  hpms_state_d |>
-  st_drop_geometry() |>
-  tibble::as_tibble() |>
-  rowwise() |>
-  mutate(
-    s2_cell = list(as.character(unlist(s2_covering_cells_buffer))),
-    .keep = "unused"
-  ) |>
-  tidyr::unnest(s2_cell) |>
-  summarize(
-    aadt_trucks_buses = mean(AADT_SINGLE_UNIT),
-    aadt_tractor_trailer = mean(AADT_COMBINATION),
-    aadt_passenger = mean(AADT) - aadt_trucks_buses - aadt_tractor_trailer,
-    .by = "s2_cell"
-  ) |>
-  mutate(s2_cell = as_s2_cell(s2_cell))
-
-tibble::tibble(
-  s2_geography = s2::s2_cell_polygon(as_s2_cell(
-    state_aadt_s2_target_lvl$s2_cell
-  )),
-  aadt = state_aadt_s2_target_lvl$aadt_passenger
-) |>
-  sf::st_as_sf() |>
-  mapview::mapview(zcol = "aadt")
-# mapview::mapview(st_as_sf(hpms_state_d), zcol = "AADT")
-
-s2_distance(
-  as_s2_geography(s2_cell_to_lnglat(state_aadt_s2_target_lvl$s2_cell)),
-  s2_union_agg(as_s2_geography(hpms_state_d$Shape))
-)
-
-# next: for query points, calculate level 21 s2 cell id and find cells within a buffer distance of point; then, design fast lookup process for s2 cell id at level 21 to table of aadt info and use to create averages of buffered inputs ......???
-
-tmp <-
-  sf::st_read(
-    dsn = gdb_path,
-    query = glue::glue(
-      "SELECT F_SYSTEM, AADT, AADT_SINGLE_UNIT, AADT_COMBINATION",
-      "FROM HPMS_FULL_{hpms_state}_2020",
-      "WHERE F_SYSTEM IN ('1', '2')",
-      .sep = " "
-    )
-  ) |>
-  sf::st_zm() |>
-  dplyr::mutate(
-    s2_geography = s2::as_s2_geography(Shape)
-  ) |>
-  sf::st_drop_geometry() |>
-  tibble::as_tibble()
-
-
-state <- "39"
-
-extract_F123_AADT <- function(state) {
-  out <-
-    sf::st_read(
-      dsn = dest_path,
+extract_F12_AADT <- function(hpms_state = "OH") {
+  hpms_state_d <-
+    st_read(
+      dsn = gdb_path,
       query = glue::glue(
-        "SELECT F_SYSTEM, AADT, AADT_SINGLE_UNIT, AADT_COMBINATION",
-        "FROM HPMS_FULL_{state}_2020",
-        "WHERE F_SYSTEM IN ('1', '2', '3')",
+        "SELECT AADT, AADT_SINGLE_UNIT, AADT_COMBINATION",
+        "FROM HPMS_FULL_{hpms_state}_2020",
+        "WHERE F_SYSTEM IN ('1', '2')",
         .sep = " "
       ),
       quiet = TRUE
     ) |>
-    sf::st_zm() |>
-    dplyr::mutate(
-      s2_geography = s2::as_s2_geography(Shape)
-    ) |>
-    sf::st_drop_geometry() |>
-    tibble::as_tibble()
-  out$length <- s2::s2_length(out$s2_geography)
-  out$s2_centroid <- purrr::map_vec(
-    out$s2_geography,
-    \(x) s2::as_s2_cell(s2::s2_centroid(x)),
-    .ptype = s2::s2_cell()
-  )
-  # out$s2_geography <- NULL
-  return(out)
+    na.omit() |>
+    st_zm() |>
+    tibble::as_tibble() |>
+    mutate(s2_geography = as_s2_geography(Shape), .keep = "unused")
+  return(hpms_state_d)
 }
 
-hpms_pa_aadt <- purrr::map(
-  hpms_states,
-  extract_F123_AADT,
-  .progress = "extracting state F123 AADT files"
-)
+all_states_aadt_f12 <-
+  purrr::map(
+    hpms_states,
+    extract_F12_AADT,
+    .progress = "extracting state F12 AADT files"
+  )
 
-out <- dplyr::bind_rows(hpms_pa_aadt)
+aadt_f12 <- bind_rows(all_states_aadt_f12)
+
+aadt_f12_sf <- st_as_sf(aadt_f12)
+
+st_write(aadt_f12_sf, "./release_me/hpms_2020_f12_aadt.gpkg", driver = "GPKG")
--- inst/traffic.R
+++ inst/traffic.R
@@ -1,136 +1,88 @@
 #' Get traffic summary
 #'
-#' Highway Performance Monitoring System (HPMS) data from 2020 is summarized as the sum of the total (and truck-only) average annual daily vehicle-meter counts within `buffer` meters of each s2 geohash.
+#' Highway Performance Monitoring System (HPMS) data from 2020 is summarized as the average daily total number of meters driven by
+#' passenger vehicles, trucks/busses, and tractor-trailers on interstates, freeways, and expressways
+#' within `buffer` meters of each s2 cell
 #' @param x a vector of s2 cell identifers (`s2_cell` object)
 #' @param buffer distance from s2 cell (in meters) to summarize data
-#' @param s2_approx_level the level of approximation used for geospatial intersections, see details
-#' @return a list the same length as `x`, which each element having a list of `total_aadt_m` and `truck_aadt_m` estimates
-#' @details By default, an s2 level 14 approximation (~ 521 m sq) is used to simplify the intersection calculation with
-#' traffic summary data. The median areas of the s2 cells in the contiguous United States by s2 level is
-#' 16: 130 m sq, 15: 260 m sq, 14: 521 m sq.
+#' @return a list the same length as `x`, which each element having a numeric vector of
+#' `aadtm_passenger`, `aadtm_trucks_buses`, `aadtm_tractor_trailer`
+#' @details
+#' Only roads with F_SYSTEM classification of 1 ("interstate") or 2 ("principal arterial - other freeways and expressways")
+#' are used. Passenger vehicles (FHWA 1-3) are calculated as the total minus FHWA class 4-7 (single unit) and 8-13 (combo)
 #' @references <https://www.fhwa.dot.gov/policyinformation/hpms.cfm>
 #' @references <https://data-usdot.opendata.arcgis.com/datasets/usdot::highway-performance-monitoring-system-hpms-2020/about>
 #' @references <https://www.fhwa.dot.gov/policyinformation/hpms/fieldmanual/hpms_field_manual_dec2016.pdf>
 #' @export
 #' @examples
 #' get_traffic_summary(
-#'   s2::as_s2_cell(c("8841b399ced97c47", "8841b38578834123")),
-#'   buffer = 1500)
-get_traffic_summary <- function(
-  x,
-  buffer = 400,
-  s2_approx_level = c("14", "15", "16")
-) {
+#'   s2::as_s2_cell(c("8841b6abd8207619", "8841b4f6affffffb", "8841b39f07f7d899")))
+get_traffic_summary <- function(x, buffer = 400) {
   check_s2_dates(x)
-  s2_approx_level <- rlang::arg_match(s2_approx_level)
-  aadt_data <-
-    readRDS(install_traffic()) |>
-    dplyr::transmute(
-      s2_centroid,
-      length,
-      aadt_total = AADT,
-      aadt_truck = sum(
-        AADT_SINGLE_UNIT,
-        AADT_COMBINATION,
-        na.rm = TRUE
-      )
-    ) |>
-    dplyr::group_by(
-      s2_parent = s2::s2_cell_parent(
-        s2_centroid,
-        level = as.numeric(s2_approx_level)
-      )
-    ) |>
-    dplyr::summarize(
-      aadt_total_m = sum(aadt_total * length, na.rm = TRUE),
-      aadt_truck_m = sum(aadt_truck * length, na.rm = TRUE)
-    )
+  hpms <- install_traffic()
   xx <- unique(x)
-  withins <- s2::s2_dwithin_matrix(
-    s2::s2_cell_to_lnglat(xx),
-    s2::s2_cell_to_lnglat(aadt_data$s2_parent),
-    distance = buffer
-  )
-  summarize_traffic <- function(i) {
-    aadt_data[withins[[i]], ] |>
-      dplyr::summarize(
-        aadt_total_m = sum(aadt_total_m),
-        aadt_truck_m = sum(aadt_truck_m)
-      ) |>
-      as.list()
+
+  x_withins <-
+    s2::s2_dwithin_matrix(
+      s2::s2_cell_center(xx),
+      hpms_state_d$s2_geography,
+      distance = buffer + 100
+    )
+
+  get_intersection_aadtm <- function(the_s2, the_s2_withins) {
+    hpms_withins <- hpms[the_s2_withins, ]
+    lengths <- s2::s2_intersection(
+      hpms_withins$s2_geography,
+      s2::s2_buffer_cells(s2::s2_cell_to_lnglat(the_s2), buffer)
+    ) |>
+      s2::s2_length()
+    out <- c(
+      aadtm_trucks_buses = sum(hpms_withins$AADT_SINGLE_UNIT * lengths),
+      aadtm_tractor_trailer = sum(hpms_withins$AADT_COMBINATION * lengths),
+      aadtm_passenger = with(
+        hpms_withins,
+        sum(
+          {
+            AADT - AADT_SINGLE_UNIT - AADT_COMBINATION
+          } *
+            lengths
+        )
+      )
+    )
+    return(out)
   }
-  withins_aadt <- purrr::map(1:length(withins), summarize_traffic)
-  names(withins_aadt) <- xx
-  return(withins_aadt[as.character(x)])
+
+  aadtm <- purrr::pmap(
+    list(the_s2 = xx, the_s2_withins = x_withins),
+    get_intersection_aadtm
+  )
+
+  names(aadtm) <- xx
+  return(aadtm[as.character(x)])
 }
 
-#' `install_traffic()` installs traffic data into user's data directory for the `appc` package
+#' `install_traffic()` installs pacakge released traffic data into user's data directory for the `appc` package
 #' @rdname get_traffic_summary
 #' @export
-install_traffic <- function(traffic_release = "hpms_f123_aadt-2024-02-04") {
+install_traffic <- function(traffic_release = "hpms_2020_f12_aadt-2025-07-16") {
   out_path <- fs::path(
     tools::R_user_dir("appc", "data"),
-    "hpms_f123_aadt",
-    ext = "rds"
+    "hpms_2020_f12_aadt",
+    ext = "gpkg"
   )
   if (file.exists(out_path)) {
     return(out_path)
   }
-  if (!install_source_preference()) {
-    dl_url <- glue::glue(
-      "https://github.com",
-      "geomarker-io",
-      "appc",
-      "releases",
-      "download",
-      traffic_release,
-      "hpms_f123_aadt.rds",
-      .sep = "/"
-    )
-    utils::download.file(dl_url, dest_file, quiet = FALSE, mode = "wb")
-    return(as.character(dest_file))
-  }
-  message("downloading and installing HPMS data from source")
-  dest_path <- tempfile(fileext = ".gdb.zip")
-  "https://www.arcgis.com/sharing/rest/content/items/c199f2799b724ffbacf4cafe3ee03e55/data" |>
-    utils::download.file(dest_path, mode = "wb")
-  hpms_states <-
-    sf::st_layers(dsn = dest_path)$name |>
-    strsplit("_", fixed = TRUE) |>
-    purrr::map_chr(3)
-  extract_F123_AADT <- function(state) {
-    out <-
-      sf::st_read(
-        dsn = dest_path,
-        query = glue::glue(
-          "SELECT F_SYSTEM, AADT, AADT_SINGLE_UNIT, AADT_COMBINATION",
-          "FROM HPMS_FULL_{state}_2020",
-          "WHERE F_SYSTEM IN ('1', '2', '3')",
-          .sep = " "
-        ),
-        quiet = TRUE
-      ) |>
-      sf::st_zm() |>
-      dplyr::mutate(
-        s2_geography = s2::as_s2_geography(Shape)
-      ) |>
-      sf::st_drop_geometry() |>
-      tibble::as_tibble()
-    out$length <- s2::s2_length(out$s2_geography)
-    out$s2_centroid <- purrr::map_vec(
-      out$s2_geography,
-      \(x) s2::as_s2_cell(s2::s2_centroid(x)),
-      .ptype = s2::s2_cell()
-    )
-    out$s2_geography <- NULL
-    return(out)
-  }
-  hpms_pa_aadt <- purrr::map(
-    hpms_states,
-    extract_F123_AADT,
-    .progress = "extracting state F123 AADT files"
+  dl_url <- glue::glue(
+    "https://github.com",
+    "geomarker-io",
+    "appc",
+    "releases",
+    "download",
+    traffic_release,
+    "hpms_2020_f12_aadt.gpkg",
+    .sep = "/"
   )
-  out <- dplyr::bind_rows(hpms_pa_aadt)
-  saveRDS(out, out_path)
+  utils::download.file(dl_url, out_path, quiet = FALSE, mode = "wb")
   return(as.character(out_path))
 }
