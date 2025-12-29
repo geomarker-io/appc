f11e3059a02aaff64faf15d957c0434e0a89b753
Cole Brokamp <cole.brokamp@gmail.com>
2025-12-29 12:41:19 -0500

    fix typo

--- R/merra.R
+++ R/merra.R
@@ -36,7 +36,7 @@
 #'   "8841b39a7c46e25f" = as.Date(c("2023-05-18", "2023-11-06")),
 #'   "8841a45555555555" = as.Date(c("2023-06-22", "2025-10-31"))
 #' )
-#' get_merra_data(x = s2::as_s2_cell(names(tmp)), dates = d)
+#' get_merra_data(x = s2::as_s2_cell(names(d)), dates = d)
 get_merra_data <- function(x, dates, merra_release = "merra-2025-12-29") {
   check_s2_dates(x, dates)
   d_merra <-
