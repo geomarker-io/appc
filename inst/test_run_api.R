d9f00277e5855ac48454f1fc55d6cf2f100fe5c5
erikarasnick <erika.rasnick@gmail.com>
2026-01-12 12:46:43 -0500

    test run api

--- inst/test_run_api.R
+++ inst/test_run_api.R
@@ -1,3 +1,3 @@
 library(plumber)
-r <- plumb("inst/plumber.R")  # Load the API definition
-r$run(port = 8000)       # Run the API on port 8000
+r <- plumb("inst/plumber.R") # Load the API definition
+r$run(port = 8000) # Run the API on port 8000
